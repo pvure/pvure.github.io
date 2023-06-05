b3cce7b1d6d1b442f55c73b0e0c867cb8f16c9f7
Aaron Hsu <aaron.hsu@duke.edu>
2023-06-05 15:03:16 -0400

    updated project descriptions

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -20,7 +20,7 @@ export default function Projects() {
                         blurb={[
                             "Currently, I'm working on creating an application that helps performers track the movement of their props with computer vision.",
                             "In the past, I've taught how to spin poi, a prop that relies on recreating patterns called flowers. It's difficult for some to visualize flowers, but since these patterns can easily be represented with parametrics, it is possible to recreate these flowers graphically.",
-                            "Computer Vision is used to extract the relative coordinates of the poi and regression is used to evaluate how accurate the patterns that performers create with their poi are to their graphical counterparts. Advice is then given to the performer to improve their accuracy.",
+                            "Computer vision is used to extract the relative coordinates of the poi and regression is used to evaluate how accurate the patterns that performers create with their poi are to their graphical counterparts. Advice is then given to the performer to improve their accuracy.",
                             "The model is built with YOLOv5 trained on a custom dataset and the computation and analysis is done in Python."
                         ]}
                         stack="Python, YOLOv5, Numpy, Matplotlib"
@@ -50,14 +50,14 @@ export default function Projects() {
 
                     blurbs={[
                         [
-                            "Quarcade is a fullstack online game platform that supports real-time interactions between users. Players can join lobbies using a generated room code, and can vote to play a fast-paced, multiplayer version of the game Bananagrams.",
+                            "Quarcade is a fullstack online game platform that supports real-time interactions between users. Players can join lobbies using a generated room code, and can vote to play a multiplayer version of the game Bananagrams.",
                             "User data is stored in MongoDB and Websockets are used to facilitate communication between users. The website wireframe was developed in Figma.",
-                            "I worked on this project through Stuyvesant's Google Mentorship program and was responsible for the front-end development of the website and managing the project task assignment. At the end of our project, we presented our project to Google employees.",
+                            "This project was developed through Stuyvesant's Google Mentorship program. At the end of our project, we presented our project to Google employees.",
                         ],
                         [
-                            "The Book Drop Notification System is an Arduino-based device that notifies librarians when it is time to collect books from the book drop.",
-                            "The system is built with multiple ESP-32 microcontrollers that communicate with each other through Firebase. Books are sensed with an ultrasound and IR beam breaker. All components are housed in a 3D-printed case. An accompanying React-based website allows librarians to monitor the status of the system.",
-                            "The system was built for the Duke University Libraries and is being used at the Perkins Library. During the school year, it is expected to reduce retrieval trips by 80% and save Duke librarians 2 hrs/wk."
+                            "This Arduino-based device installed in Duke University's Perkins Library notifies librarians when a bin is full of books.",
+                            "The system is built with multiple ESP-32 microcontrollers that post and retrieve data from Firebase. Books are sensed with an ultrasound and IR beam breaker and all components are housed in a custom-designed 3D-printed case. An accompanying React-based website allows librarians to monitor the status of the system.",
+                            "The system was built for the Duke University Libraries. During the school year, it is expected to reduce retrieval trips by 80% and save Duke librarians 2 hrs/wk."
                         ]
                     ]}
 
