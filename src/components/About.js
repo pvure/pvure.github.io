5376defb2ba2c0f8a92bcdf2599341f77c311311
Aaron Hsu <aaron.hsu@duke.edu>
2023-09-17 13:22:17 -0400

    edit bio

--- src/components/About.js
+++ src/components/About.js
@@ -20,7 +20,7 @@ export default function About() {
                         "I’m a sophomore at Duke University studying CS and Electrical/Computer Engineering. My background is in algorithms (USACO Gold) and web development but recently, I’ve been exploring deep learning and developing programs for microcontrollers.",
                         "Last summer ('23), I was a Software Engineering Intern at Hotplate, a Series A startup backed by Y Combinator, where I designed and developed a data pipeline to scrape and analyze Instagram accounts for market intelligence purposes.",
                         "The previous summer, I was an Atlas Fellow and worked at Cisco to develop an engineering solution to improve the connected rail system in the United States.",
-                        "I’m interested in entrepreneurship/startups and am looking to learn how to build scalable infrastructure and applications on a team."
+                        "I’m interested in entrepreneurship/startups and am looking to build scalable infrastructure and applications on a team."
                     ]}
                 />
             </div>
