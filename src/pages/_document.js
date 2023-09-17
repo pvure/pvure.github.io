33e820ab4de7f81834f592d10cb4782b2de42c05
Aaron Hsu <aaron.hsu@duke.edu>
2023-09-17 13:20:04 -0400

    improve seo

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,6 +4,9 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+        <title>Aaron Hsu</title>
+        <link rel="icon" href="/fire.ico" /> 
+        <meta name="description" content="Personal website of Aaron Hsu, a sophomore at Duke University studying Computer Science and Electrical/Computer Engineering." />
       <body>
         <Main />
         <NextScript />
